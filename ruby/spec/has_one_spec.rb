4570e5164a79d30225931fb22295df55e3060750
cmamani <cmamani@hexacta.com>
2023-05-12 00:07:11 -0300

    feat: added Boolean module

--- ruby/spec/has_one_spec.rb
+++ ruby/spec/has_one_spec.rb
@@ -1,16 +1,24 @@
 require "rspec"
 require_relative '../lib/Persistence'
+require_relative '../lib/Boolean'
 
 describe 'has_one tests' do
     before do
       class Raccoon
         include Persistence
         has_one String, named: :name
+        has_one Boolean, named: :is_Avenger
+
+        attr_accessor :energy
       end
       @one_raccoon = Raccoon.new
     end
 
     it 'Raccoon define atributo name con has_one' do
       expect(@one_raccoon).to have_attributes(:name => nil)
+    end
+
+    it 'Raccoon define atributo booleano con has_one' do
+      expect(@one_raccoon).to have_attributes(:is_Avenger => nil)
     end
 end
