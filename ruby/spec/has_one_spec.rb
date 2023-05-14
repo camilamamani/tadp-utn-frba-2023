7d61532da0a19aa2ea3a97938a1b8740452cc206
cmamani <cmamani@hexacta.com>
2023-05-14 16:13:27 -0300

    test: added test in has_one

--- ruby/spec/has_one_spec.rb
+++ ruby/spec/has_one_spec.rb
@@ -2,12 +2,13 @@ require "rspec"
 require_relative '../lib/Persistence'
 require_relative '../lib/Boolean'
 
-describe 'has_one tests' do
+describe 'Persistence Tests' do
+  context '1. has_one' do
     before do
       class Raccoon
         include Persistence
         has_one String, named: :name
-        has_one Boolean, named: :is_Avenger
+        has_one Boolean, named: :is_avenger
         has_one String, named: :age
         has_one Numeric, named: :age
 
@@ -20,11 +21,16 @@ describe 'has_one tests' do
     end
 
     it 'Raccoon define atributo booleano con has_one' do
-      expect(@one_raccoon).to have_attributes(:is_Avenger => nil)
+      expect(@one_raccoon).to have_attributes(:is_avenger => nil)
     end
 
     it 'Raccoon setea y lee sus atributos de forma normal' do
       @one_raccoon.name = "Rocket Raccoon"
       expect(@one_raccoon.name).to eq("Rocket Raccoon")
     end
+    it 'Raccoon setea y lee sus atributos de forma normal' do
+      @one_raccoon.is_avenger = true
+      expect(@one_raccoon.is_avenger).to eq(true)
+    end
+  end
 end
