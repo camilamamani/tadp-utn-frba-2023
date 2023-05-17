127944b64b7a724362633ebd42049e323f8c9f6c
cmamani <cmamani@hexacta.com>
2023-05-16 22:18:28 -0300

    test: added test when getting all instances and updating one

--- ruby/spec/all_instances_spec.rb
+++ ruby/spec/all_instances_spec.rb
@@ -9,23 +9,38 @@ describe 'AllInstances' do
     before do
       class Student
         include Persistence
-        has_one String, named: :full_name
+        has_one String, named: :name
         has_one Numeric, named: :age
         has_one Boolean, named: :is_regular
       end
       @one_student = Student.new
+      @one_student.name = "Alejandro"
+      @one_student.age  = 25
+      @one_student.save!
     end
 
     after do
       TADB::DB.clear_all
     end
 
-    it 'returns all instances of a class' do
-      @one_student.full_name = "Alejandro"
-      @one_student.age  = 25
+    it 'returns all instances of a class that are saved' do
+      @other_student = Student.new
+      @other_student.name = "Fernando"
+      @other_student.age  = 30
+      @other_student.save!
+
+      expect(Student.all_instances.size).to eq(2)
+    end
+
+    it 'all instances returned are actual classes and can be updated' do
+      @one_student.name = "Pablo"
       @one_student.save!
 
-      expect(Student.all_instances.size).to eq(1)
+      @student_a = Student.all_instances.first
+      @student_a.name = "Student A"
+      @student_a.save!
+
+      expect(Student.all_instances.first.name).to eq("Student A")
     end
   end
 end
