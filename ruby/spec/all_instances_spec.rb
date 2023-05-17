bbf52e0047338299df4e3efd19e3a901ef4e03ca
cmamani <cmamani@hexacta.com>
2023-05-16 23:24:01 -0300

    fix: test to compare students

--- ruby/spec/all_instances_spec.rb
+++ ruby/spec/all_instances_spec.rb
@@ -12,35 +12,48 @@ describe 'AllInstances' do
         has_one String, named: :name
         has_one Numeric, named: :age
         has_one Boolean, named: :is_regular
+        def ==(other)
+          self.class == other.class && name == other.name && age == other.age && is_regular == other.is_regular
+        end
       end
       @one_student = Student.new
       @one_student.name = "Alejandro"
       @one_student.age  = 25
       @one_student.save!
+
+      @other_student = Student.new
+      @other_student.name = "Fernando"
+      @other_student.age  = 30
+      @other_student.save!
+
     end
 
     after do
       TADB::DB.clear_all
     end
 
-    it 'returns all instances of a class that are saved' do
-      @other_student = Student.new
-      @other_student.name = "Fernando"
-      @other_student.age  = 30
-      @other_student.save!
-
+    it 'returns all instances of a students that are saved' do
       expect(Student.all_instances.size).to eq(2)
     end
 
-    it 'all instances returned are actual classes and can be updated' do
-      @one_student.name = "Pablo"
-      @one_student.save!
+    it 'returns all instances of a students that are saved except the ones forgot' do
+      @other_student.forget!
+      expect(Student.all_instances.size).to eq(1)
+    end
 
-      @student_a = Student.all_instances.first
+    it 'all instances returned are actual classes and can be updated' do
+      @student_a = Student.all_instances[0]
       @student_a.name = "Student A"
       @student_a.save!
 
-      expect(Student.all_instances.first.name).to eq("Student A")
+      expected_student = Student.new
+      expected_student.id= @student_a.id
+      expected_student.name= @student_a.name
+      expected_student.age=@student_a.age
+      expected_student.is_regular= @student_a.is_regular
+      expect(Student.all_instances.include?(expected_student)).to eq(true)
+      expect(Student.all_instances.size).to eq(2)
     end
+
   end
 end
