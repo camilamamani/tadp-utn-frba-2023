88ad08aaff36c33f66a83292404769cc632f010d
MatiasRosbaco <mati.rosbaco@gmail.com>
2023-06-27 13:33:13 -0300

    agregando los parsers

--- scala/src/main/scala/Parser.scala
+++ scala/src/main/scala/Parser.scala
@@ -11,3 +11,49 @@ case object AnyChar extends Parser[Char] {
     case head :: tail => Success(Result(head, tail.mkString("")))
   }
 }
+
+def char(expectedChar: Char)(text: String): Parser[Char] = {
+  text.headOption match {
+    case Some(actualChar) if actualChar == expectedChar =>
+      Success(actualChar)
+    case _ =>
+      Failure(new Exception(s"Se esperaba el carácter '$expectedChar'"))
+  }
+}
+def void(text: String): Try[Unit] = {
+  text.headOption match {
+    case Some(_) => Success(())
+    case None => Failure(new Exception("El texto está vacío"))
+  }
+}
+
+def letter(text: String): Parser[Char] = {
+  text.headOption match {
+    case Some(char) if char.isLetter => Success(char)
+    case _ => Failure(new Exception("No se encontró un carácter válido"))
+  }
+}
+
+def digit(text: String): Parser[Char] = {
+  text.headOption match {
+    case Some(char) if char.isDigit => Success(char)
+    case _ => Failure(new Exception("No se encontró un dígito válido"))
+  }
+}
+
+def alphaNum(text: String): Parser[Char] = {
+  letter(text) match {
+    case Success(char) => Success(char)
+    case Failure(_) => digit(text) match {
+      case Success(char) => Success(char)
+      case Failure(_) => Failure(new Exception("No se encontró un carácter alfanumérico válido"))
+    }
+  }
+}
+//uso parser aca??? nos conviene normalizar(quitarle espacios al text y al expected)
+def string(expected: String, text: String): Try[String] = {
+  text.startsWith(expected) match {
+    case true => Success(expected)
+    case false => Failure(new Exception("No se encontró el string esperado"))
+  }
+}
