94f462cfbb68b2e9504feded22c453532814b8aa
cmamani <cmamani@hexacta.com>
2023-07-07 23:59:05 -0300

    added sonido parsers and tests, refactor parser

--- scala/src/main/scala/Parser.scala
+++ scala/src/main/scala/Parser.scala
@@ -1,28 +1,30 @@
-import javax.swing.JToolBar.Separator
-import scala.Array.copy
-import scala.collection.mutable.ListBuffer
 import scala.util.{Failure, Success, Try}
 
-abstract class Parser[T] {
+abstract class Parser[+T] {
   def apply(input: String): Try[Result[T]]
 
-  def <|>(oneParser: Parser[T]): Parser[T] = {
-    (input: String) =>
-      this.apply(input) match {
+  def <|>[R >: T](oneParser: Parser[R]): Parser[R] = {
+    val thisParser = this
+    (input: String) => {
+      thisParser.apply(input) match {
         case Success(Result(head, tail)) => Success(Result(head, tail))
         case Failure(_) => oneParser.apply(input)
       }
+    }
   }
-  def <>[S](oneParser: Parser[S]): Parser[(T, S)] = {
-    (input: String) => this.apply(input) match {
+
+  def <>[R >: T, S](oneParser: Parser[S]): Parser[(R, S)] = {
+    val thisParser = this
+    (input: String) => {
+      thisParser.apply(input) match {
         case Success(Result(consumed1, tail)) => oneParser.apply(tail) match {
           case Success(Result(consumed2, tail)) => Success(Result((consumed1, consumed2), tail))
           case Failure(_) => Failure(new ConcatCombinatorException)
         }
         case Failure(_) => Failure(new ConcatCombinatorException)
       }
+    }
   }
-
   def ~>[S](oneParser: Parser[S]): Parser[S] = {
     (input: String) => {
       (this <> oneParser).map(elem => elem._2)(input)
@@ -38,7 +40,6 @@ abstract class Parser[T] {
       case Failure(_) => Failure(new MapException)
     }
   }
-
   def satisfies(function: T => Boolean): Parser[T] = {
     (input: String) =>
       this.apply(input) match {
@@ -47,7 +48,6 @@ abstract class Parser[T] {
         case Failure(_) => Failure(new SatisfiesException)
       }
   }
-
   def opt(): Parser[Option[T]] = {
     (input: String) =>
       this.apply(input) match {
@@ -72,7 +72,6 @@ abstract class Parser[T] {
   }
 
   def +(): Parser[List[T]] = {
-
       this.*()
   }
   def sepBy[S](separatorParser: Parser[S]): Parser[List[T]] = {
@@ -82,11 +81,14 @@ abstract class Parser[T] {
     }
   }
 
-  def const[S](constValue: S): Parser[S] = {
-    (input: String) => this.apply(input) match {
-      case Success(Result(_, tail)) => Success(Result(constValue, tail))
-      case Failure(exc) => Failure(exc)
+  def const[S >: T](constValue: S): Parser[S] = {
+    val thisParser = this
+    (input: String) => {
+      thisParser.apply(input) match {
+        case Success(Result(_, tail)) => Success(Result(constValue, tail))
+        case Failure(exc) => Failure(exc)
+      }
     }
   }
 }
-case class Result[T](parsedInput: T, unparsedInput: String)
+case class Result[+T](parsedInput: T, unparsedInput: String)
--- scala/src/test/scala/TocablesParsersTests.scala
+++ scala/src/test/scala/TocablesParsersTests.scala
@@ -22,4 +22,46 @@ class TocablesParsersTests extends AnyFlatSpec{
     silencio("").failure.exception shouldBe a[EmptyInputStringException]
   }
 
+  "nota Parser C" should " parsear correctamente" in {
+    parserNota("C") shouldBe Success(Result(C, ""))
+  }
+  "nota Parser Cs" should " parsear correctamente" in {
+    parserNota("Cs") shouldBe Success(Result(Cs, ""))
+  }
+  "nota Parser B#" should " parsear correctamente" in {
+    parserNota("B#") shouldBe Success(Result(As, ""))
+  }
+  "nota Parser Bs" should " parsear correctamente" in {
+    parserNota("Bs") shouldBe Success(Result(C, ""))
+  }
+
+  "tono Parser 4C" should " parsear correctamente" in {
+    parserTono("4C") shouldBe Success(Result(Tono(4,C), ""))
+  }
+  "tono Parser Cs" should " parsear correctamente" in {
+    parserTono("5Cs") shouldBe Success(Result(Tono(5, Cs), ""))
+  }
+  "tono Parser B#" should " parsear correctamente" in {
+    parserTono("4B#") shouldBe Success(Result(Tono(4, As), ""))
+  }
+  "tono Parser Bs" should " parsear correctamente" in {
+    parserTono("5Bs") shouldBe Success(Result(Tono(5,C), ""))
+  }
+
+  "figura Parser redonda" should " parsear correctamente" in {
+    parserFigura("1/1") shouldBe Success(Result(Redonda, ""))
+  }
+  "figura Parser semiCorchea" should " parsear correctamente" in {
+    parserFigura("1/16") shouldBe Success(Result(SemiCorchea, ""))
+  }
+
+  "sonido Parser 4C1/1" should " parsear correctamente" in {
+    parserSonido("4C1/1") shouldBe Success(Result(Sonido(Tono(4,C),Redonda), ""))
+  }
+  "sonido Parser 6A#1/4" should " parsear correctamente" in {
+    parserSonido("6A#1/4") shouldBe Success(Result(Sonido(Tono(6, Gs), Negra), ""))
+  }
+  "sonido Parser 6Z#1/1" should " parsear correctamente" in {
+    parserSonido("6Z#1/1").failure.exception shouldBe a[MapException]
+  }
 }
